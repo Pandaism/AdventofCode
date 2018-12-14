6b6578671a82f4b070038b47a52d596ae0c18536
Mike Nguyen <mike94.nguyen@yahoo.com>
2018-12-14 12:16:12 -0600

    Completed Day 11 Part 1 Completed

---
+++ Chronal Charge/src/me/advent/part1/ChronalCharge.java
@@ -0,0 +1,113 @@
+package me.advent.part1;
+
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Pandaism
+ * @date 12/13/2018 : 8:07 PM
+ */
+public class ChronalCharge {
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        File file = Paths.get(ClassLoader.getSystemResource("PuzzleInput.txt").toURI()).toFile();
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String serial = reader.readLine();
+
+        int gridSize = 300;
+        int[][] grid = new int[gridSize][gridSize];
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[i].length; j++) {
+                int power;
+                int rackID = (i + 1) + 10;
+                int startingPower = rackID * (j + 1);
+                power = startingPower + Integer.parseInt(serial);
+                power *= rackID;
+                if(power < 1000) {
+                    power /= 100;
+                } else {
+                    String sPower = String.valueOf(power);
+                    char h = sPower.charAt(sPower.length() - 3);
+                    power = Integer.parseInt(String.valueOf(h));
+                }
+
+                power -= 5;
+
+                grid[j][i] = power;
+            }
+        }
+
+        Map<Coordinates, Integer> powerLevel = new HashMap<>();
+        for(int y = 0; y < grid.length; y++) {
+            for(int x = 0; x < grid[y].length; x++) {
+                if(x + 3 > grid[y].length) {
+                    break;
+                }
+
+                if(y + 3 > grid.length) {
+                    break;
+                }
+
+                int power = 0;
+                for(int i = y; i < y + 3; i++) {
+                    for(int j = x; j < x + 3; j++) {
+                        power += grid[i][j];
+                    }
+                }
+
+                powerLevel.put(new Coordinates(x + 1, y + 1), power);
+            }
+        }
+
+        Coordinates ans = null;
+        for(Coordinates coords : powerLevel.keySet()) {
+            if(ans == null) {
+                ans = coords;
+            } else {
+                if(powerLevel.get(coords) > powerLevel.get(ans)) {
+                    ans = coords;
+                }
+            }
+
+        }
+
+        if(ans != null) System.out.printf("%d,%d\n", ans.x, ans.y);
+
+        /*
+        To find fuel cell level
+        Example: Cell (3, 5) with serial 8
+
+            Find the fuel cell's rack ID, which is its X coordinate plus 10.
+                The rack ID is 3 + 10 = 13
+
+            Begin with a power level of the rack ID times the Y coordinate.
+                The power level starts at 13 * 5 = 65
+
+            Increase the power level by the value of the grid serial number (your puzzle input).
+                Adding the serial number produces 65 + 8 = 73
+
+            Set the power level to itself multiplied by the rack ID.
+                Multiplying by the rack ID produces 73 * 13 = 949
+
+            Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
+                The hundreds digit of 949 is 9
+
+            Subtract 5 from the power level.
+                Subtracting 5 produces 9 - 5 = 4
+
+         The power level of this fuel cell is 4.
+         */
+    }
+
+    static class Coordinates {
+        private int x;
+        private int y;
+
+        Coordinates(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
